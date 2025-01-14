5e6300b066f48711f6bc5bbce8899dc700017d78
arben-djokovic <benidjokovic12@gmail.com>
2025-01-14 18:23:45 +0100

    Todo desc validator and changed api url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://todo-app-mern-stack-h1p7.onrender.com/api',
   headers: {
     "Content-Type": "application/json"
   }
--- frontend/src/pages/TodoForm/EditTodo.js
+++ frontend/src/pages/TodoForm/EditTodo.js
@@ -1,28 +1,32 @@
-import React, { useEffect, useRef, useState } from 'react'
-import './todoForm.scss'
-import api from '../../api/api';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import React, { useEffect, useRef, useState } from "react";
+import "./todoForm.scss";
+import api from "../../api/api";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 export default function EditTodo() {
   const [todo, setTodo] = useState({});
   const { id } = useParams();
   const formRef = useRef(null);
   const titleErrorRef = useRef(null);
+  const descriptionErrorRef = useRef(null);
   const navigate = useNavigate();
 
   const editTodo = async () => {
     const title = formRef.current.title.value;
     const description = formRef.current.description.value;
-    titleErrorRef.current.style.display = 'none'
-    if(title.length < 2 || title.length > 30) return titleErrorRef.current.style.display = 'block';
-
-    try{
-      await api.put(`/todos/${id}`, {title, description});
-      navigate('/');
-    }catch(err){
-      console.log(err)
+    titleErrorRef.current.style.display = "none";
+    descriptionErrorRef.current.style.display = "none";
+    if (title.length < 2 || title.length > 30)
+      return (titleErrorRef.current.style.display = "block");
+    if (description.length > 50)
+      return (descriptionErrorRef.current.style.display = "block");
+    try {
+      await api.put(`/todos/${id}`, { title, description });
+      navigate("/");
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
 
   useEffect(() => {
     const getTodo = async () => {
@@ -32,29 +36,58 @@ export default function EditTodo() {
       } catch (err) {
         console.error(err.message);
       }
-    }
-    getTodo()
-  }, [])
-
-
+    };
+    getTodo();
+  }, []);
 
   return (
-    <div className='page formTodoPage'>
-      <Link className='homeLink' to="/"><i class="fa fa-arrow-left" aria-hidden="true"></i></Link>
-      <section className='container'>
-        <h1 className="title"><span>Edit Todo</span><i className='fa fa-edit' aria-hidden="true"></i></h1>
-        <form ref={formRef} className='todoForm' onSubmit={(e) => e.preventDefault()}>
-          <div className='input'>
-            <p>Title <span ref={titleErrorRef} className='error'>Title must be beetwen 2 and 30 characters</span></p>
-            <input defaultValue={todo.title} type="text" name='title' placeholder="Title" />
+    <div className="page formTodoPage">
+      <Link className="homeLink" to="/">
+        <i class="fa fa-arrow-left" aria-hidden="true"></i>
+      </Link>
+      <section className="container">
+        <h1 className="title">
+          <span>Edit Todo</span>
+          <i className="fa fa-edit" aria-hidden="true"></i>
+        </h1>
+        <form
+          ref={formRef}
+          className="todoForm"
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <div className="input">
+            <p>
+              Title{" "}
+              <span ref={titleErrorRef} className="error">
+                Title must be beetwen 2 and 30 characters
+              </span>
+            </p>
+            <input
+              defaultValue={todo.title}
+              type="text"
+              name="title"
+              placeholder="Title"
+            />
           </div>
-          <div className='input'>
-            <p>Description <span className='error'></span></p>
-            <input defaultValue={todo.description} type="text" name='description' placeholder="Description" />
+          <div className="input">
+            <p>
+              Description
+              <span ref={descriptionErrorRef} className="error">
+                Description must be at most 50 characters.
+              </span>
+            </p>
+            <input
+              defaultValue={todo.description}
+              type="text"
+              name="description"
+              placeholder="Description"
+            />
           </div>
-          <button onClick={editTodo} className='addTodo'>Edit Todo</button>
+          <button onClick={editTodo} className="addTodo">
+            Edit Todo
+          </button>
         </form>
       </section>
-    </div> 
-  )
+    </div>
+  );
 }
